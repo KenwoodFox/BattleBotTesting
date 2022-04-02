3477acd171950db62b259274f7a973a0b1b48003
KenwoodFox <kenwood364@gmail.com>
2022-04-02 16:00:18 -0400

    Recompile firmware for ESP32-cam

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -7,18 +7,19 @@
 // Arduino Libs
 #include "Arduino.h"
 
+#define FLASH_LED 4
 
 void setup()
 {
     // initialize digital pin LED_BUILTIN as an output.
-    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(FLASH_LED, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop()
 {
-    digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
-    delay(1000);                     // wait for a second
-    digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
-    delay(1000);                     // wait for a second
+    digitalWrite(FLASH_LED, HIGH); // turn the LED on (HIGH is the voltage level)
+    delay(1000);                   // wait for a second
+    digitalWrite(FLASH_LED, LOW);  // turn the LED off by making the voltage LOW
+    delay(1000);                   // wait for a second
 }
